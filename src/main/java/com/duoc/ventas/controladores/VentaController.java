8b991dd81bd33cd72b5e296b53c9618fa2b73b61
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-09-21 23:41:55 -0300

    Adding controllers

--- src/main/java/com/duoc/ventas/controladores/VentaController.java
+++ src/main/java/com/duoc/ventas/controladores/VentaController.java
@@ -1,12 +1,11 @@
 package com.duoc.ventas.controladores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.duoc.ventas.modelos.VentaProducto;
 import com.duoc.ventas.servicios.VentaService;
@@ -15,13 +14,38 @@ import com.duoc.ventas.servicios.VentaService;
 @RestController
 @RequestMapping("/ventas")
 public class VentaController {
+
     @Autowired
     private VentaService ventaService;
 
     //ver todas las ventas
     @GetMapping
     public List<VentaProducto> obtenerTodasLasVentas() {
-        return ventaService.obtenerTodasLasVentas();
+        return ventaService.obtenerVentas();
+    }
+
+    //ver venta por id
+    @GetMapping("/{id}")
+    public Optional<VentaProducto> obtenerVentaPorId(@PathVariable Long id){
+        return  ventaService.obtenerPorId(id);
+    }
+
+    //guardar venta
+    @PostMapping
+    public VentaProducto guardarVenta(@RequestBody VentaProducto ventaProducto){
+        return ventaService.guardarVenta(ventaProducto);
+    }
+
+    //modificar
+    @PutMapping("/{id}")
+    public ResponseEntity<VentaProducto> actualizarVenta(@PathVariable Long id, @RequestBody VentaProducto ventaProducto){
+        return ventaService.actualizarVenta(id, ventaProducto);
+    }
+
+    //eliminar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarVenta(@PathVariable Long id){
+        return ventaService.eliminarVenta(id);
     }
 
     //ver ganancia del dia
@@ -32,13 +56,13 @@ public class VentaController {
 
     //ver ganancia del mes
     @GetMapping("/ganancias/mes/{mes}/{anio}")
-    public double obtenerGananciasMensuales(@PathVariable int mes, int anio) {
-        return ventaService.obtenerGananciasMensuales(mes, anio);   
+    public double obtenerGananciasMensuales(@PathVariable int mes, @PathVariable int anio) {
+        return ventaService.obtenerGananciasPorMes(mes, anio);
     }
 
     //ver ganancia del año
     @GetMapping("/ganancias/anio/{anio}")
     public double obtenerGananciasAnuales(@PathVariable int anio) {
-        return ventaService.obtenerGananciasAnuales(anio);  
+        return ventaService.obtenerGananciasPorAnio(anio);
     }
 }
